cea3ec6bf5479b71d1e4b035eaafcdbed031cbfa
emanoelcampos <emanoel.cv@gmail.com>
2023-06-30 05:18:17 -0300

    apagando atributos e métodos desnecessários da classe que mapeia o json

---
+++ src/main/java/dev/emanoel/converter/currency/model/Currency.java
@@ -0,0 +1,14 @@
+package dev.emanoel.converter.currency.model;
+
+public class Currency {
+
+    private String code;
+    private String codein;
+    private String name;
+    private double bid;
+
+    public double getBid() {
+        return bid;
+    }
+
+}
