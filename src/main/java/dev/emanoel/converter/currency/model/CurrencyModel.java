9e4f706d289658202f1db9a7974f38f7575f3449
emanoelcampos <emanoel.cv@gmail.com>
2023-06-30 23:11:52 -0300

    classe modelo reduzida apenas para atributos necessários

--- src/main/java/dev/emanoel/converter/currency/model/CurrencyModel.java
+++ src/main/java/dev/emanoel/converter/currency/model/CurrencyModel.java
@@ -2,9 +2,6 @@ package dev.emanoel.converter.currency.model;
 
 public class CurrencyModel {
 
-    private String code;
-    private String codein;
-    private String name;
     private double bid;
 
     public double getBid() {
