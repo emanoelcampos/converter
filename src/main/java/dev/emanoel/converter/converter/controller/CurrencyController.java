a74526d244721247a3bf9bd907c1025dd3c62bc0
emanoelcampos <emanoel.cv@gmail.com>
2023-07-07 15:57:30 -0300

    implementação de comentários e retirada do try-catch adicionando um throws no método

--- src/main/java/dev/emanoel/converter/converter/controller/CurrencyController.java
+++ src/main/java/dev/emanoel/converter/converter/controller/CurrencyController.java
@@ -4,6 +4,9 @@ import dev.emanoel.converter.converter.service.CurrencyService;
 
 import java.io.IOException;
 
+/**
+ * Classe responsável por controlar a lógica de conversão de moedas.
+ */
 public class CurrencyController {
 
     private static final String API_URL = "https://economia.awesomeapi.com.br/last/BRL-USD,BRL-EUR,BRL-GBP,BRL-ARS,BRL-CLP,USD-BRL,USD-EUR,USD-GBP,USD-ARS,USD-CLP,EUR-BRL,EUR-USD,EUR-GBP,EUR-ARS,EUR-CLP,GBP-BRL,GBP-USD,GBP-EUR,ARS-BRL,ARS-USD,ARS-EUR,CLP-BRL,CLP-USD";
@@ -14,14 +17,17 @@ public class CurrencyController {
         this.currencyService = new CurrencyService();
     }
 
-    public double processExchange(String currencies, double value){
-        try {
-            return currencyService.calculateExchange(API_URL, currencies, value);
-        } catch (IOException e) {
-            throw new RuntimeException("Erro de IO ao obter cotações de moeda. Verifique sua conexão de internet e tente novamente.", e);
-        } catch (InterruptedException e) {
-            throw new RuntimeException("A solicitação de cotações de moeda foi interrompida. Tente novamente mais tarde.", e);
-        }
+    /**
+     * Processa a conversão de moeda com base nas moedas e no valor fornecidos.
+     *
+     * @param currencies As moedas a serem usadas na conversão.
+     * @param value O valor a ser convertido.
+     * @return O resultado da conversão.
+     * @throws IOException se ocorrer um erro de I/O durante a chamada à API
+     * @throws InterruptedException se a thread for interrompida durante a chamada à API
+     */
+    public double processExchange(String currencies, double value) throws IOException, InterruptedException {
+        return currencyService.calculateExchange(API_URL, currencies, value);
     }
 
 }
