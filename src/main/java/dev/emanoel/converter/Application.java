8c05e187dfb1c8c41eea63e828214027b8d50002
emanoelcampos <emanoel.cv@gmail.com>
2023-07-07 16:05:31 -0300

    classe da aplicação

---
+++ src/main/java/dev/emanoel/converter/Application.java
@@ -0,0 +1,15 @@
+package dev.emanoel.converter;
+
+
+import javax.swing.*;
+
+public class Application {
+
+    public static void main(String[] args) {
+
+        SwingUtilities.invokeLater(() -> {
+            MainFrame window = new MainFrame();
+            window.showMenu();
+        });
+    }
+}
