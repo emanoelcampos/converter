0c894c1623a4c2178ce1862eb3b9b775f9ee55f9
emanoelcampos <emanoel.cv@gmail.com>
2023-07-07 13:00:06 -0300

    opção de temperatura avisando que ainda está em desenvolvimento

---
+++ src/main/java/dev/emanoel/converter/view/temperature/TemperatureFrame.java
@@ -0,0 +1,11 @@
+package dev.emanoel.converter.view.temperature;
+
+import javax.swing.*;
+
+public class TemperatureFrame {
+
+    public void temperatureMenu() {
+        JOptionPane.showMessageDialog(null, "Em desenvolvimento!",
+                "Temperatura", JOptionPane.WARNING_MESSAGE);
+    }
+}
