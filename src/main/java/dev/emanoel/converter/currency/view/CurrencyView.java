8a75aa2d13bdbfccaa88abed48ba6ed1bf440725
emanoelcampos <emanoel.cv@gmail.com>
2023-07-10 14:42:28 -0300

    atributo adicionado para tornar a classe mais flexivel

--- src/main/java/dev/emanoel/converter/currency/view/CurrencyView.java
+++ src/main/java/dev/emanoel/converter/currency/view/CurrencyView.java
@@ -7,15 +7,17 @@ import dev.emanoel.converter.view.Converter;
  */
 public class CurrencyView implements Converter {
 
+    private final String currencyName;
     private CurrencyProcessResult currencyProcessResult;
 
-    public CurrencyView( ) {
+    public CurrencyView() {
         this.currencyProcessResult = new CurrencyProcessResult();
+        this.currencyName = "Conversor de Moedas";
     }
 
     @Override
     public String getName() {
-        return "Conversor de Moedas";
+        return currencyName;
     }
 
     @Override
@@ -25,17 +27,23 @@ public class CurrencyView implements Converter {
 
     public void showCurrencyMenu() {
 
+        //tela de entrada de valor para conversão
         double inputValue = CurrencyInputView.promptValue();
 
+        //tela de opçÕes de escolha de de moedas para conversão
         String inputCurrencyChoice = CurrencySelectionView.promptCurrency();
 
+        //retorna as moedas e o simbolo com base na escolha de moedas para conversão
         String currencies = CurrencyChoiceUtil.getCurrencies(inputCurrencyChoice);
         String currencySymbol = CurrencyChoiceUtil.getCurrencySymbol(inputCurrencyChoice);
 
+        //guarda o valor da conversão recebendo as moedas e o valor para calculo da conversão
         double exchangeResult = currencyProcessResult.performConversion(currencies, inputValue);
 
+        //tela de resultado com a conversão feita e com o simbolo da moeda
         CurrencyResultView.showExchangeResult(exchangeResult, currencySymbol);
 
+        //tela de confirmação para decidir o que fazer após a conversão
         CurrencyConfirmationView.showConfirmationMenu();
     }
 }
